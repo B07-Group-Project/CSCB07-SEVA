8e8055e76114b0631616ca4494782ba585aa3e06
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-07-28 18:18:32 -0400

    Update Class Structures

--- app/src/main/java/com/utsc/project/Event.java
+++ app/src/main/java/com/utsc/project/Event.java
@@ -1,24 +1,46 @@
 package com.utsc.project;
 
+import java.util.HashSet;
+
 public class Event {
-    User [] attendees; //should these be private?
+    int id;
+    HashSet<User> attendees; //should these be private?
     User creator;
     int maxPlayers;
     String startTime; //might change type in future
     String endTime;
     Venue venue;
 
-    public Event(User[] attendees, User creator, int maxPlayers, String startTime, String endTime,
-                 Venue venue) {
-        this.attendees = attendees;
+    public Event(User creator, int maxPlayers, String startTime, String endTime,
+                 Venue venue, int id) {
         this.creator = creator;
         this.maxPlayers = maxPlayers;
         this.startTime = startTime;
         this.endTime = endTime;
         this.venue = venue;
+        this.id = id;
+        this.attendees = new HashSet<>();
+    }
+
+     public void addAttendee(String id) {
+        this.attendees.add(new User(id));
+    }
+
+    public void removeAttendee(String id) {
+        this.attendees.remove(new User(id));
     }
 
     public int getUserCount() {
-        return attendees.length;
+        return attendees.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Event) {
+            Event e = (Event)o;
+            return e.id == this.id;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/utsc/project/User.java
+++ app/src/main/java/com/utsc/project/User.java
@@ -1,13 +1,24 @@
 package com.utsc.project;
 
 import java.util.HashSet;
+import java.util.Objects;
 
 public class User {
-    int id;
+    String id;
     HashSet<Event> createdEvents;
     HashSet<Event> joinedEvents;
 
-    public User(int id) {
+    public User(String id) {
         this.id = id;
+        this.createdEvents = new HashSet<>();
+        this.joinedEvents = new HashSet<>();
+    }
+    public boolean equals(Object o) {
+        if (o instanceof User) {
+            User u = (User)o;
+            return Objects.equals(this.id, u.id);
+        } else {
+            return false;
+        }
     }
 }
