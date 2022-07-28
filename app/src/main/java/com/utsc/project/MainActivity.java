20fdc7074b75715099a99b3c22293b83da9f9fff
amyli123 <amyamay22@gmail.com>
2022-07-27 21:45:55 -0400

    Update package name in manifest

--- app/src/main/java/com/utsc/project/MainActivity.java
+++ app/src/main/java/com/utsc/project/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.project.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
