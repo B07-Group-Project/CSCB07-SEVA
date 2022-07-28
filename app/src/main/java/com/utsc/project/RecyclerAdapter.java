b7254cfea1e1c04e25664a1e3370eac816da4f13
amyli123 <amyamay22@gmail.com>
2022-07-28 18:48:08 -0400

    Implement functions in RecyclerViewAdapter and add components to list_my_events

--- app/src/main/java/com/utsc/project/RecyclerAdapter.java
+++ app/src/main/java/com/utsc/project/RecyclerAdapter.java
@@ -1,25 +1,48 @@
 package com.utsc.project;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private ArrayList<Event> eventsList;
+
+    public RecyclerAdapter(ArrayList<Event> myEvents) {
+        this.eventsList = myEvents;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView eventName;
+
+        public MyViewHolder(final View view) {
+            super(view);
+            this.eventName = view.findViewById(R.id.textView);
+        }
+    }
 
     @NonNull
     @Override
     public RecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_my_events, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
-
+        //String name = eventsList.get(position).getName();
+        //holder.eventName.setText(name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return eventsList.size();
     }
 }
