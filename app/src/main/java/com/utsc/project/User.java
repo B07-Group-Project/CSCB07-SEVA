aead2d17b68ff2e2bd08e1902cbc4d96d4ba58a5
amyli123 <amyamay22@gmail.com>
2022-07-28 00:57:09 -0400

    Add extra fields to classes

--- app/src/main/java/com/utsc/project/User.java
+++ app/src/main/java/com/utsc/project/User.java
@@ -1,7 +1,12 @@
 package com.utsc.project;
 
+import java.util.HashSet;
+
 public class User {
     int id;
+    HashSet<Event> createdEvents;
+    HashSet<Event> joinedEvents;
+
     public User(int id) {
         this.id = id;
     }
