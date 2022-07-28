71421bcbf04d0a9b2932e4e1b5810c24d4fe3fc1
amyli123 <amyamay22@gmail.com>
2022-07-28 14:08:47 -0400

    Create activity for displaying my events

---
+++ app/src/main/java/com/utsc/project/DisplayMyEventsActivity.java
@@ -0,0 +1,14 @@
+package com.utsc.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMyEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_my_events);
+    }
+}
---
+++ app/src/main/java/com/utsc/project/RecyclerAdapter.java
@@ -0,0 +1,25 @@
+package com.utsc.project;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
