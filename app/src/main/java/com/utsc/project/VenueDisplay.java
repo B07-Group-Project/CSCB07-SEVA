ba1bcd677d6644c9f4548d07eba2426fb0dd1f8a
Noah Cristino <noahcristino@gmail.com>
2022-07-28 20:46:34 -0400

    added VenueDisplay activity

---
+++ app/src/main/java/com/utsc/project/VenueDisplay.java
@@ -0,0 +1,14 @@
+package com.utsc.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VenueDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_display);
+    }
+}
