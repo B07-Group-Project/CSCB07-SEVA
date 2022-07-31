a0512aba3230357292e37018d2f394c377fd6100
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-07-31 12:07:52 -0400

    Add 'eventTypes' feild to Venue.java

--- app/src/main/java/com/utsc/project/Venue.java
+++ app/src/main/java/com/utsc/project/Venue.java
@@ -1,14 +1,19 @@
 package com.utsc.project;
 
+import java.util.ArrayList;
+
 public class Venue {
     String name;
     int id;
     int courts;
+    ArrayList<String> eventTypes;
+
 
     public Venue(String name, int id, int courts) {
         this.name = name;
         this.id = id;
         this.courts = courts;
+        this.eventTypes = new ArrayList<>();
     }
 
     public String getName() {
