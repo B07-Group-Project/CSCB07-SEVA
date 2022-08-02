586311606890251f3c9e9a8798cdaf5bb2920899
Jiayoqin <728981622@qq.com>
2022-08-02 18:24:35 -0400

    add register regex and firebase

--- app/src/main/java/com/utsc/project/Register_page.java
+++ app/src/main/java/com/utsc/project/Register_page.java
@@ -3,12 +3,74 @@ package com.utsc.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Register_page extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register_page extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private TextView register;
+    private EditText username, password, c_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        register = (Button) findViewById(R.id.button3);
+
+        username = (EditText) findViewById(R.id.editTextTextPersonName);
+        password = (EditText) findViewById(R.id.editTextTextPassword2);
+        c_password = (EditText) findViewById(R.id.editTextTextPassword3);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button3) {
+            register_user();
+        }
+
+    }
+
+    private void register_user() {
+        String name = username.getText().toString().trim();
+        String pw = password.getText().toString().trim();
+        String cpw = c_password.getText().toString().trim();
+
+        if(name.isEmpty()){
+            username.setError("User name cannot be empty!");
+            username.requestFocus();
+            return;
+        }
+
+        if(pw.isEmpty()){
+            password.setError("Password cannot be empty!");
+            password.requestFocus();
+            return;
+        }
+
+        if(!pw.equals(cpw)){
+            c_password.setError("Password mismatch");
+            c_password.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            password.setError("Password needs to be at least 6 characters!");
+            password.requestFocus();
+            return;
+        }
+
+        DatabaseReference ref =
+                FirebaseDatabase.getInstance("https://b07project-e4016-default-rtdb.firebaseio.com"
+                ).getReference();
+        User user = new User(name, pw);
+        ref.child("Users").child(name).setValue(user);
     }
 }
--- app/src/main/java/com/utsc/project/User.java
+++ app/src/main/java/com/utsc/project/User.java
@@ -1,13 +1,22 @@
 package com.utsc.project;
 
+import com.google.firebase.database.Exclude;
+
 import java.util.HashSet;
 
 public class User {
-    int id;
+    String id;
+    String password;
+    @Exclude
     HashSet<Event> createdEvents;
+    @Exclude
     HashSet<Event> joinedEvents;
 
-    public User(int id) {
-        this.id = id;
+
+    public User(){}
+
+    public User(String username, String password){
+        this.id = username;
+        this.password = password;
     }
 }
