14e043061d7224f7a165a3ab60a11cd2fc5d0d23
amyli123 <amyamay22@gmail.com>
2022-08-05 20:00:28 -0400

    Revert "added input fields for venue name and # of courts to add venue layout"
    This reverts commit a74042e533faa2e265bd99a8a14d70c28ed494ef.

--- app/src/main/java/com/utsc/project/AdminAddVenueFragment.java
+++ app/src/main/java/com/utsc/project/AdminAddVenueFragment.java
@@ -59,6 +59,6 @@ public class AdminAddVenueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_add_venue, container, false);
+        return inflater.inflate(R.layout.fragment_add_venue, container, false);
     }
 }
